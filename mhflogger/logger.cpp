0dc1f856cbe27148fef205d99efdf88eb2336c0e
megadrifter <megadrifter@gmail.com>
2016-08-17 14:45:46 +0300

    added csv file output each 900 lines and time [NOT TESTED]

--- mhflogger/logger.cpp
+++ mhflogger/logger.cpp
@@ -1,7 +1,15 @@
+/* TODO 
+запуск и остановка по <???>
+при стопе запись остатков
+формат данных: csv
+класс mhfLogger? (начать в отдельном файле .н)
+*/
+
 // all the startup stuff with headers
 #include <cstdlib>
 #include <iostream>
-// later #include <fstream> 
+#include <fstream> 
+#include <ctime>
 
 #include <RF24/RF24.h> // use installed library
 
@@ -11,6 +19,28 @@ RF24 radio(RPI_V2_GPIO_P1_22, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
 
 int main(int argc, char** argv)
 {
+/* при старте создаётся новый лог-файл, в имени - время (как треки гпс) */
+/** prepare the name of current file, including name (path and name as parameter?) with date **/
+
+char nowtime[32];
+time_t t = time(0);
+strftime(nowtime, sizeof(nowtime), "%Y-%m-%d-%H-%M-%S", localtime(&t));
+cout << nowtime << '\n';
+
+char* file_name = /*nowtime*/ "mhflog.csv"; // формат год-месяц-день-час-мин-сек-mhflog.csv
+/*FILE *file;
+file = fopen( file_name, "w" ); 
+fputs( "Begin \n", file );
+fclose( file );*/
+ofstream file;  
+file.open ("Test.txt", ios::out);
+file << "Begin \n"; // << endl
+      file  << "BpemR"  << ";"
+            <<  "V" << ";"
+            <<  "A" << ";
+            <<  "H" << ";"
+            <<  "Ah" << "\n";
+file.close;
 
 
 /** Setup mhf **/
@@ -30,33 +60,57 @@ radio.startListening(); // ПОЕХАЛИ! ~O+=
 /* this seems to be enough. */ 
 
 /** set up variables **/
-char receivePayload[33]; // container for payload + "\0"
+int bufrows = 900; // how many rows in the buffer
+char receivePayload[bufrows][33]; // container for payload + "\0"
+time_t timesec[bufrows];
 
 radio.printDetails(); // see all settings
 cout<<"ПОЕХАЛИ! ~O+= \n";
 
+
 // waiting (or not) for cought packets k times and printing them
 while(1)
   { 
-    delay(90); // Data is transmitted 5 times/ sec. Wait some time.
-    
-    if (radio.available())
+    for (int i=0; i<bufrows; ) // counter if recieved
     {
-      /** Clear measurement values **/ 
-      memset(receivePayload,0,32);
-      radio.read(receivePayload, 32);
-      /** print payload bytes separatelly in decimal format **/
+      delay(90); // Data is transmitted 5 times/ sec. Wait some time.
+      // вставить обработчик завершения?
       
-      for (int digit=0; digit < 32; ++digit)
-          printf("%d ", (int)(receivePayload[digit])); 
-      cout <<"\r"; // make a single row
+      if (radio.available())
+      {
+        /** Clear measurement values **/ 
+        memset(receivePayload,0,32);
+        /** write to a memory buffer **/ 
+        radio.read(receivePayload[i], 32);
+        
+        // напечатать дату и время
+        // cout << date[i] = ;
+        timesec[i] = time(0);
+        
+        /** print payload bytes separatelly in decimal format **/
+        for (int digit=0; digit < 32; ++digit)
+            printf("%d ", (int)(receivePayload[i][digit])); 
+        cout <<"\r"; // make a single row
+        ++i; 
+      }
       
-      /** write to a memory buffer **/ 
-    
+    }
+    /** append to a file  **/
+    /* file = fopen( file_name, "a" ); 
+    char thestring = receivePayload[i];
+    fputs( thestring, file );*/
+    file.open (file_name, ios::app);
+    for (int i=0; i<bufrows; ++i) 
+    {
+      strftime(nowtime, sizeof(nowtime), "%Y-%m-%d-%H-%M-%S", localtime(&timesec[i]));
+      file  <<  nowtime << ";"
+            <<  receivePayload[i][1] << ";"
+            <<  receivePayload[i][3] << ";"
+            <<  receivePayload[i][5] << ";"
+            <<  receivePayload[i][7] << "\n";
     }
     
-      /** append to a file if size is...  or time interval...  **/
-      
+    file.close;
     
   }
 
